7aa31232ea21dcf0c9233b7cd8581220bcf8550f
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-06-07 17:06:24 -0300

    initiated project

---
+++ src/main/java/br/com/dicasdeumdev/apitestes/ApiTestesApplication.java
@@ -0,0 +1,13 @@
+package br.com.dicasdeumdev.apitestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dicasdeumdev/apitestes/ApiTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dicasdeumdev.apitestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
