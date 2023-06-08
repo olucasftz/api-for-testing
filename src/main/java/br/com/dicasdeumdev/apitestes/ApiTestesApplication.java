05400c1a79571b4f3251467d9d0f0b40d8ea8f5b
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-06-08 11:33:56 -0300

    Revert "feat: User class"
    This reverts commit cfba2e1b36d3d1d47be9cfe18e96fd5e2eac2b23.

--- src/main/java/br/com/dicasdeumdev/apitestes/ApiTestesApplication.java
+++ src/main/java/br/com/dicasdeumdev/apitestes/ApiTestesApplication.java
@@ -1,12 +1,13 @@
 package br.com.dicasdeumdev.apitestes;
 
-import br.com.dicasdeumdev.apitestes.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiTestesApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTestesApplication.class, args);
 	}
+
 }
