cfba2e1b36d3d1d47be9cfe18e96fd5e2eac2b23
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-06-08 11:23:40 -0300

    feat: User class

--- src/main/java/br/com/dicasdeumdev/apitestes/ApiTestesApplication.java
+++ src/main/java/br/com/dicasdeumdev/apitestes/ApiTestesApplication.java
@@ -1,13 +1,12 @@
 package br.com.dicasdeumdev.apitestes;
 
+import br.com.dicasdeumdev.apitestes.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiTestesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTestesApplication.class, args);
 	}
-
 }
