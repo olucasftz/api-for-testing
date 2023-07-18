4f6986e08d6de11f7864e5a354568c9433ff6cac
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-07-18 11:46:22 -0300

    feat: exception for email already registered

--- src/main/java/br/com/dicasdeumdev/apitestes/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/dicasdeumdev/apitestes/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.apitestes.resources.exceptions;
 
+import br.com.dicasdeumdev.apitestes.services.exceptions.DataIntegratyViolationException;
 import br.com.dicasdeumdev.apitestes.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,5 +17,12 @@ public class ResourceExceptionHandler {
         StandardError error =
                 new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), exception.getMessage(), status.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegratyViolationException.class)
+    public ResponseEntity<StandardError> dataIntegratyViolation(DataIntegratyViolationException exception, HttpServletRequest status) {
+        StandardError error =
+                new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), exception.getMessage(), status.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/br/com/dicasdeumdev/apitestes/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/dicasdeumdev/apitestes/services/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.dicasdeumdev.apitestes.domain.User;
 import br.com.dicasdeumdev.apitestes.domain.UserDTO;
 import br.com.dicasdeumdev.apitestes.repositories.UserRepository;
 import br.com.dicasdeumdev.apitestes.services.UserService;
+import br.com.dicasdeumdev.apitestes.services.exceptions.DataIntegratyViolationException;
 import br.com.dicasdeumdev.apitestes.services.exceptions.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(UserDTO obj) {
+        findByEmail(obj);
         return userRepository.save(mapper.map(obj, User.class));
+    }
+
+    private void findByEmail(UserDTO obj) {
+        Optional<User> emailOfUser = userRepository.findByEmail(obj.getEmail());
+        if (emailOfUser.isPresent()) {
+            throw new DataIntegratyViolationException("email already registered in the system!");
+        }
     }
 }
