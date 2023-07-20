55a3d8b4c70180c773eb52507cec943cc4caf58b
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-07-20 15:39:04 -0300

    test: test for method finById in user class on exception line of not found

--- src/test/java/br/com/dicasdeumdev/apitestes/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/dicasdeumdev/apitestes/services/impl/UserServiceImplTest.java
@@ -3,7 +3,8 @@ package br.com.dicasdeumdev.apitestes.services.impl;
 import br.com.dicasdeumdev.apitestes.domain.User;
 import br.com.dicasdeumdev.apitestes.domain.UserDTO;
 import br.com.dicasdeumdev.apitestes.repositories.UserRepository;
-import org.junit.jupiter.api.Assertions;
+import br.com.dicasdeumdev.apitestes.resources.UserController;
+import br.com.dicasdeumdev.apitestes.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -27,6 +28,7 @@ class UserServiceImplTest {
     public static final String  NAME     = "Valdir";
     public static final String  EMAIL    = "valdir@gmail.com";
     public static final String  PASSWORD = "123";
+    public static final String OBJECT_NOT_FOUND = "Object not found";
 
     @InjectMocks
     private UserServiceImpl service;
@@ -55,6 +57,18 @@ class UserServiceImplTest {
         assertEquals(ID,response.getId());
         assertEquals(NAME,response.getName());
         assertEquals(EMAIL,response.getEmail());
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnObjectNotFoundException() {
+        when(repository.findById(Mockito.anyInt())).thenThrow(new ObjectNotFoundException(OBJECT_NOT_FOUND));
+
+        try {
+            service.findById(ID);
+        } catch (Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(OBJECT_NOT_FOUND, ex.getMessage());
+        }
     }
 
     @Test
