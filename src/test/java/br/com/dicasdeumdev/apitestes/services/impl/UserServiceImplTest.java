9712ef5dbc664cb8db566b56966f98dba205c2d5
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-07-21 09:52:39 -0300

    test: test on findAll service method in user class

--- src/test/java/br/com/dicasdeumdev/apitestes/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/dicasdeumdev/apitestes/services/impl/UserServiceImplTest.java
@@ -14,6 +14,7 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -29,6 +30,7 @@ class UserServiceImplTest {
     public static final String  EMAIL    = "valdir@gmail.com";
     public static final String  PASSWORD = "123";
     public static final String OBJECT_NOT_FOUND = "Object not found";
+    public static final int INDEX = 0;
 
     @InjectMocks
     private UserServiceImpl service;
@@ -72,7 +74,19 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUsers() {
+        when(repository.findAll()).thenReturn(List.of(user));
+
+        List<User> response = service.findAll();
+
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(User.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getId());
+        assertEquals(NAME, response.get(INDEX).getName());
+        assertEquals(EMAIL, response.get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.get(INDEX).getPassword());
     }
 
     @Test
@@ -88,7 +102,7 @@ class UserServiceImplTest {
     }
 
     private void startUser(){
-        user= new User(ID, NAME, EMAIL, PASSWORD);
+        user = new User(ID, NAME, EMAIL, PASSWORD);
         userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
         optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
     }
